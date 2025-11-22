74b4720bd8512dfb2c59a92f064efa8c4b5593ca
Miguel Vieira <152816803+MiguelOlivieira@users.noreply.github.com>
2025-11-22 14:20:38 -0300

    feat: add denuncia model and setup initil view for user creation

--- denuncIA/models.py
+++ denuncIA/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Denuncia(models.Model):
+    categoria = models.CharField(max_length=100)
+    descricao = models.TextField()
+    localizacao = models.CharField(max_length = 200)
+    status = models.CharField(max_length = 10)
+    foto = models.ImageField(upload_to="caminho_futuro/")
+    autor = models.ForeignKey(User, on_delete=models.Cascade, related_name="denuncias")
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.categoria
